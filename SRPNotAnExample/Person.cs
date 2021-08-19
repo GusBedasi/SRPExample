2efde8a34912749e6c98e5d0bfb1f7b6cfb574f8
Gustavo Bedasi <ggustavobedasi@gmail.com>
2021-08-18 21:56:28 -0300

    added the reverse example of SRP (#3)

---
+++ SRPNotAnExample/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SRPNotAnExample
+{
+    public class Person
+    {
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SRPNotAnExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SRPNotAnExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+
+            var user = new Person();
+            
+            Console.WriteLine("What is your first name?");
+            user.FisrtName = Console.ReadLine();
+            
+            Console.WriteLine("What is your last name?");
+            user.LastName = Console.ReadLine();
+            
+            if (string.IsNullOrWhiteSpace(user.FisrtName))
+            {
+                Console.WriteLine($" You did not give us a valid first name");
+                Console.ReadLine();;
+            }
+
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine($" You did not give us a valid last name!");
+                Console.ReadLine();
+            }
+            
+            Console.WriteLine($"Welcome {user.FisrtName.Substring(0, 3) + user.LastName}!");
+            Console.ReadLine();
+        }
+    }
+}
